e1983f80eec7197fadd96d518447811e553e0e15
vyerru <jrakha55@gmail.com>
2025-10-23 01:14:20 +0700

    first-commit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\Site\SiteController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/halamanutama', [SiteController::class, 'index'])->name('halamanutama');
